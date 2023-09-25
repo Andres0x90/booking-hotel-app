6c051bedc703a5dd79def761515f950d7a965bc7
Edwin Palacios <edwin.palacios@correo.tdea.edu.co>
2023-09-25 02:40:21 -0500

    test DeleteBooking y HistoryPerRoom finalizados

--- src/test/java/co/edu/tdea/domain/services/BookingServiceImplTest.java
+++ src/test/java/co/edu/tdea/domain/services/BookingServiceImplTest.java
@@ -8,13 +8,14 @@ import co.edu.tdea.domain.models.Room;
 import co.edu.tdea.domain.models.Types;
 import co.edu.tdea.infrastructure.data.BookingData;
 import co.edu.tdea.infrastructure.data.ClientData;
+import co.edu.tdea.infrastructure.data.FineData;
 import co.edu.tdea.infrastructure.repositories.BookingRepository;
 import co.edu.tdea.infrastructure.repositories.RoomRepository;
-import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.core.*;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.subscribers.TestSubscriber;
 import junit.framework.TestCase;
+import org.hibernate.mapping.Any;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -23,9 +24,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookingServiceImplTest extends TestCase {
@@ -35,6 +40,7 @@ public class BookingServiceImplTest extends TestCase {
     @Mock
     BookingRepository bookingRepository;
 
+    @Mock
     Mapper mapper = new Mapper();
 
     @InjectMocks
@@ -42,7 +48,7 @@ public class BookingServiceImplTest extends TestCase {
 
     @Test
     public void testCreateRoom() {
-        Mockito.when(roomRepository.save(Mockito.any())).thenReturn(new Room());
+        when(roomRepository.save(Mockito.any())).thenReturn(new Room());
         bookingService.createRoom(RoomDTO.builder().id(1).type("EXECUTIVE").build())
                 .test().assertComplete();
     }
@@ -52,7 +58,7 @@ public class BookingServiceImplTest extends TestCase {
         bookingService.setMapper(mapper);
         Mockito.when(roomRepository.findById(1)).thenReturn(Optional.of(new Room(1, Types.EXECUTIVE)));
         Mockito.when(bookingRepository.save(Mockito.any())).thenReturn(BookingData.builder()
-                        .client(ClientData.builder().document("123").fines(List.of()).build())
+                .client(ClientData.builder().document("123").fines(List.of()).build())
                 .build());
 
         bookingService.bookRoom(Booking.builder()
@@ -65,9 +71,76 @@ public class BookingServiceImplTest extends TestCase {
                 .assertComplete();
     }
 
+    @Test
     public void testDeleteBooking() {
+        // Arrange
+        String bookingCode = "001";
+        BookingData bookingData = new BookingData(); // Crea una instancia de BookingData con los datos necesarios
+        bookingData.setStartDate(java.util.Date.from(LocalDate.now().minusDays(4).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+        bookingData.setFee(BigDecimal.valueOf(100.0));
+        ClientData clientDataMock = Mockito.mock(ClientData.class);
+        //clientData.setFines(new ArrayList<>());
+        bookingData.setClient(clientDataMock);
+
+        when(bookingRepository.findById(bookingCode)).thenReturn(Optional.of(bookingData));
+
+        Completable result = bookingService.deleteBooking(bookingCode);
+
+        // Assert
+        result.test()
+                .assertComplete();
+
+        verify(bookingRepository, times(1)).findById(bookingCode);
+
+        verify(bookingRepository, times(1)).deleteById(bookingCode);
     }
 
+    @Test
     public void testGetHistoryPerRoom() {
+        String bookingCode = "001";
+        BookingData bookingData = new BookingData(); // Crea una instancia de BookingData con los datos necesarios
+        bookingData.setStartDate(java.util.Date.from(LocalDate.now().minusDays(4).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+        bookingData.setFee(BigDecimal.valueOf(100.0));
+        ClientData clientDataMock = Mockito.mock(ClientData.class);
+        //clientData.setFines(new ArrayList<>());
+        bookingData.setClient(clientDataMock);
+
+        // Arrange
+        Integer roomId = 1;
+        List<BookingData> bookingDataList = List.of(
+                bookingData
+        );
+
+        when(bookingRepository.findAllByRoomId(roomId)).thenReturn(bookingDataList);
+
+        // Supongamos que tu mapper convierte BookingData en Booking
+        when(mapper.toEntity(bookingData)).thenAnswer(invocation -> {
+            BookingData nBookingData = invocation.getArgument(0);
+            if (nBookingData == null) {
+                // Maneja el caso en el que bookingData es nulo
+                // Puedes lanzar una excepción o tomar la acción adecuada
+                throw new IllegalArgumentException("bookingData no debe ser nulo");
+            }
+            // Verifica que bookingData no sea nulo antes de acceder a sus propiedades
+            String code = nBookingData.getCode(); // Acceder a getCode() después de la verificación de nulidad
+            Booking booking = new Booking();
+            // Configura la conversión de BookingData a Booking según tus necesidades
+            return booking;
+        });
+
+        // Act
+        Flowable<Booking> result = bookingService.getHistoryPerRoom(roomId);
+
+        // Assert
+        result.test()
+                .assertComplete()
+                .assertValueCount(bookingDataList.size());
+
+        // Verifica que se llamó a findAllByRoomId con el roomId correcto
+        verify(bookingRepository, times(1)).findAllByRoomId(roomId);
+
+        // Verifica que se llamó a mapper.toEntity para cada BookingData en la lista
+        verify(mapper, times(bookingDataList.size())).toEntity(any(BookingData.class));
     }
+
 }
